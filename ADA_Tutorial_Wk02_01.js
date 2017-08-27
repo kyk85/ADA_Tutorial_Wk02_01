11946bc68808cf2f9ca0f7e1acf6c9906e331f5a
Khoo Yit Keng <30973917+kyk85@users.noreply.github.com>
2017-08-27 13:59:15 +0800

    Added TryAgain

--- ADA_Tutorial_Wk02_01.js
+++ ADA_Tutorial_Wk02_01.js
@@ -1,29 +1,41 @@
 var randomNumber='';
 var userNumber='';
 var chances=3;
+var answer=document.getElementById('answer')
+var chancesLeft=document.getElementById('chancesLeft')
+var clickme=document.getElementById('clickme')
+var tryagain=document.getElementById('tryagain')
 
 function generateNumber() {
 	randomNumber=Math.floor((Math.random()*100)+1);
+	tryagain.style.display='none'
 	console.log(randomNumber);
 }
-
 
 function testThisNumber() {
 	userNumber=document.getElementById('testNumber').value;
 	if (chances>0) {
 		if (userNumber==randomNumber) {
-		document.getElementById('answer').innerHTML="Congratulations! You guessed right!";
-		document.getElementById('chancesLeft').innerHTML="You had " + chances + " chance(s) left!";
+		answer.innerHTML="Congratulations! You guessed right!";
+		chancesLeft.innerHTML="You had " + chances + " chance(s) left!";
+		clickme.style.display='none'
+		tryagain.style.display=''
 		return;
 		} else if (userNumber>randomNumber) {
-		document.getElementById('answer').innerHTML="Your guess of " + userNumber + " is too high!";
+		answer.innerHTML="Your guess of " + userNumber + " is too high!";
 		} else if (userNumber<randomNumber) {
-		document.getElementById('answer').innerHTML="Your guess of " + userNumber + " is too low!";
+		answer.innerHTML="Your guess of " + userNumber + " is too low!";
 		}
 		chances--;
-		document.getElementById('chancesLeft').innerHTML="You have " + chances + " tries left!";
+		chancesLeft.innerHTML="You have " + chances + " tries left!";
 	} else {
-		document.getElementById('answer').innerHTML="You have run out of guesses! Reload the page to try again!";
-		document.getElementById('chancesLeft').innerHTML=":(";
+		answer.innerHTML="You have run out of guesses! The right number is " + randomNumber + ". Reload the page to try again!";
+		chancesLeft.innerHTML=":(";
+		clickme.style.display='none';
+		tryagain.style.display='';
 	}
+}
+
+function reloadpage() {
+	location.reload();
 }
